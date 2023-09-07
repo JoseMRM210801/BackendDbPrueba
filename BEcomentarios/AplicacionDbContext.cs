a48004f03ef7c6a19367afa171fcd50c75406a5d
JoseMRM210801 <s19120164@alumnos.itsur.edu.mx>
2023-09-07 16:18:08 -0600

    creaciòn del backend de la base de datos

---
+++ BEcomentarios/AplicacionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BEcomentarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BEcomentarios
+{
+    public class AplicacionDbContext : DbContext
+    {
+        public DbSet<Comentario>Comentario{ get; set; }
+        public AplicacionDbContext(DbContextOptions<AplicacionDbContext> options): base(options)
+        {
+
+        }
+        
+    }
+}
